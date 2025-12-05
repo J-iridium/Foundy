e74a5b0be5fa10d7e8276d8cd51fd0581fe4037b
jiri132 <76393975+jiri132@users.noreply.github.com>
2025-12-05 21:30:58 +0100

    global utils of pipeline

---
+++ src/lib/sdk/v3/pipeline/05-hydrate/utils/resolveData.ts
@@ -0,0 +1,7 @@
+export function resolveData(rootData: any, path?: string[]): any {
+  if (!path || path.length === 0) return rootData;
+
+  return path.reduce((acc, key) => {
+    return (acc && acc[key] !== undefined) ? acc[key] : undefined;
+  }, rootData);
+}
